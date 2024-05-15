ce030d2e4c8d5b6593027f7b3883374d491fd4dc
Dhanush S Gowda <141866615+Dhanush-S-Gowda@users.noreply.github.com>
2024-05-15 22:22:19 +0530

    Add files via upload

---
+++ armstrong_api.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def greet():
+    return "welcome to armstrong number api"
+
+@app.route('/armstrong/<int:n>')
+def armstorng_num(n):
+    sum = 0
+    order = len(str(n))
+    copy_n = n
+    while(n>0):
+        digit = n%10
+        sum+= digit **order
+        n = n//10
+    
+    if(sum == copy_n):
+        result = {
+            "number": copy_n,
+            "is Armstrong": True
+        }
+    else:
+        result = {
+            "number": copy_n,
+            "is Armstrong": False
+        }
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
